26c8c48d2ae9df057238c427a1261b9e910bbfe6
Nicolas GOMBERT <gombert.n@gmail.com>
2019-05-03 18:10:44 +0200

    .gitignore fix

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const livebox_1 = require("./models/livebox");
-const lb = new livebox_1.Livebox("192.168.1.2", "8080");
-// const cmd: liveBoxCmd = {operation: "01", key: "513", mode: "0"};
-const cmd = { operation: "10" };
-console.log(lb.getUrl(cmd));
-lb.currentContext().then((data) => console.log(data));
-//# sourceMappingURL=app.js.map
